78ffcc8d9939f3982eb35d1362c887597f6b2ec6
ritchie46 <ritchie46@gmail.com>
2023-04-04 07:34:59 +0000

    Deploying to gh-pages from @ pola-rs/polars@da67abe14ac5d4bb8ddcfb86bad02a497ebd96bf 🚀

--- polars_core/prelude/stringcache/sidebar-items.js
+++ polars_core/prelude/stringcache/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["reset_string_cache","toggle_string_cache","using_string_cache","with_string_cache"],"struct":["IUseStringCache"]};
+window.SIDEBAR_ITEMS = {"fn":["enable_string_cache","reset_string_cache","using_string_cache","with_string_cache"],"struct":["IUseStringCache"]};
