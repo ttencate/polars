1e53d124d2abcafedfd2fbc18a20ac152e5c1841
ritchie46 <ritchie46@gmail.com>
2023-04-11 09:56:00 +0000

    Deploying to gh-pages from @ pola-rs/polars@4b0f984b7b59c4313343d708cacce3f6644f6199 🚀

--- implementors/arrow2/array/trait.MutableArray.js
+++ implementors/arrow2/array/trait.MutableArray.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {
 "arrow2":[],
-"polars":[]
+"polars":[],
+"polars_arrow":[["impl <a class=\"trait\" href=\"arrow2/array/trait.MutableArray.html\" title=\"trait arrow2::array::MutableArray\">MutableArray</a> for <a class=\"struct\" href=\"polars_arrow/array/null/struct.MutableNullArray.html\" title=\"struct polars_arrow::array::null::MutableNullArray\">MutableNullArray</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
