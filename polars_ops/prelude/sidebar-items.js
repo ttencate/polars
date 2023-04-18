d87a7a7e474d21d1d7fc3130d529677eba14b31b
ritchie46 <ritchie46@gmail.com>
2023-04-18 18:19:32 +0000

    Deploying to gh-pages from @ pola-rs/polars@4d730e579f84c52a8e9f1b1dbdb2b3f762f81f18 🚀

--- polars_ops/prelude/sidebar-items.js
+++ polars_ops/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SearchSortedSide"],"fn":["floor_div_series","is_first","search_sorted"],"trait":["ArgAgg","DataFrameJoinOps","DataFrameOps","RollingSeries","SeriesMethods","SeriesSealed","ToDummies"]};
+window.SIDEBAR_ITEMS = {"enum":["SearchSortedSide"],"fn":["floor_div_series","is_first","search_sorted"],"trait":["ArgAgg","DataFrameJoinOps","DataFrameOps","LogSeries","RollingSeries","SeriesMethods","SeriesSealed","ToDummies"]};
