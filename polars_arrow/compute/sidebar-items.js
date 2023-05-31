251878d985181ef46b237ad8aed778f4c50e4764
ritchie46 <ritchie46@gmail.com>
2023-05-31 07:42:51 +0000

    Deploying to gh-pages from @ pola-rs/polars@98adaf274b5890786eb7114a4c040c367e78ea13 🚀

--- polars_arrow/compute/sidebar-items.js
+++ polars_arrow/compute/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["bitwise","cast","decimal","take","tile"]};
+window.SIDEBAR_ITEMS = {"fn":["binary_mut","unary_mut"],"mod":["arithmetics","arity","bitwise","cast","decimal","take","tile"]};
